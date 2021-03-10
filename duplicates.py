7423017fd0abe8227112e03274403bdafa538bc7
Raphael9999 <rlouvrier@outlook.com>
2021-03-10 11:01:08 -0600

    refactor: convert to positive logic, improve print
    * instead of using continue on failed condition,
    proceed on successfull condition
    * print out errors during hashing process
    * rephrase completion message

--- duplicates.py
+++ duplicates.py
@@ -97,17 +97,17 @@ def get_files_by_full(hashes_on_1k, hash=hashlib.sha1):
     # For all files with the same file size, and hash on the 1st 1024 bytes
     hashes_full = defaultdict(list)   # dict of full_file_hash: full_path_to_file_string
     for k, files_list in hashes_on_1k.items():
-        if len(files_list) < 2:
-            continue    # this hash of fist 1k file bytes is unique, no need to spend cpy cycles on it
-
-        for filename in files_list:
-            try:
-                full_hash = get_hash(filename, first_chunk_only=False)
-                hashes_full[(full_hash, k[1])].append(filename)
-            except (OSError,):
-                # the file access might've changed till the exec point got here 
-                continue
-    print('List full hash, done')
+        if len(files_list) > 1:
+            # we have several potential duplicated files that need further analysis
+            for filename in files_list:
+                try:
+                    full_hash = get_hash(filename, first_chunk_only=False)
+                    hashes_full[(full_hash, k[1])].append(filename)
+                except (OSError,):
+                    # problem accesing the file
+                    print(f'Error hashing file: {filename}')
+                    continue # continue the loop
+    print('Dictionary by full hash has been built')
     return hashes_full
 
 def is_in_dir(file_name=None, folder=None):
