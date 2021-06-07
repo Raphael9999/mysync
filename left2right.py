37ead2366d929199c4046952a339fd8ba3676912
Raphael9999 <rlouvrier@outlook.com>
2021-06-07 15:22:16 -0500

    fix: add try except to handle some special folder and file name

--- left2right.py
+++ left2right.py
@@ -17,9 +17,13 @@ def get_files_and_size(path:str) -> pd.DataFrame:
         # get all files that have the same size - they are the collision candidates
         for filename in filenames:
             full_path = os.path.join(dirpath, filename)
+            try:
+                _size = os.path.getsize(full_path)
+            except:
+                _size = 0
             new_row = {'short': full_path.replace(path, '', 1).lstrip('\\').lstrip('//'), 
                        'path': full_path, 
-                       'size': os.path.getsize(full_path)}
+                       'size': _size}
             _df = _df.append(new_row, ignore_index=True)
     _df.set_index('short', drop=True, inplace=True)
     return _df
@@ -35,28 +39,40 @@ def copy_left_to_right(source, target, test:bool=True):
         if short in target_files.index:
             if src_row['size'] != target_files.loc[short, 'size']:
                 # we overwrite the file in the target folder
-                print(f"Source and target file have different size, copy from source to target for: {short}")
+                
                 if not test:
-                    shutil.copy2(src_row['path'], target_files.loc[short, 'path'])
+                    try: 
+                        shutil.copy2(src_row['path'], target_files.loc[short, 'path'])
+                        print(f"Source and target file have different size, copy from source to target for: {short}")
+                    except:
+                        print(f"fail to copy from source to target for: {short}")
             else: 
                 print(f"Source and target file have same size, no action for: {short}")
         else:
             # source file is not in target, we copy it
-            print(f"Source file is not in target folder, copy from source to target for: {short}")
+            
             if not test:
                 trg_path = os.path.join(target, short)
                 trg_dir = os.path.dirname(trg_path)
-                if not os.path.isdir(os.path.dirname(trg_path)):
-                    os.makedirs(trg_dir)
-                shutil.copy2(src_row['path'], trg_path)
+                if not os.path.isdir(trg_dir):
+                    try:
+                        os.makedirs(trg_dir)
+                    except:
+                        print(f"fail to create folder {trg_dir}")
+                try:
+                    shutil.copy2(src_row['path'], trg_path)
+                    print(f"Source file is not in target folder, copy from source to target for: {short}")
+                except:
+                    print(f"fail to copy from source to target for: {short}")
+
     
 
 # root directory of the source directories and files
-source = r'C:\Users\Raphael.Louvrier\OneDrive - Vallourec\3 Personal Development'
+source = r'C:\Users\Raphael.Louvrier\OneDrive - Vallourec\1 Support'
 
 # root directory of the target directories and files, 
 # the structure is expected to be the same as in the source
-target = r'D:\3 Personal Development'
+target = r'D:\1 Support'
 
-# Done 3, 5
+# Done , 2, 3, 5
 copy_left_to_right(source, target, False)
